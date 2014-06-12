1c297bdcc7a5b3ffa09e769e8656726dd6b01a47
Tim Underwood <timunderwood@gmail.com>
2014-06-12 14:36:39 -0700

    ByteBufferInputStream can now take multiple ByteBuffers and will wrap them in a ByteBufferInputStream and then a SequenceInputStream to return a combined InputStream

--- src/main/scala/fm/common/ByteBufferInputStream.scala
+++ src/main/scala/fm/common/ByteBufferInputStream.scala
@@ -15,13 +15,40 @@
  */
 package fm.common
 
-import java.io.InputStream
-import java.nio.ByteBuffer
+import java.io.{InputStream, IOException, SequenceInputStream}
+import java.nio.{ByteBuffer, InvalidMarkException}
+import scala.collection.JavaConverters._
+
+object ByteBufferInputStream {
+  /**
+   * This takes combined multiple ByteBuffers into a single InputStream by
+   * wrapping them in ByteBufferInputStreams and then using a
+   * java.io.SequenceInputStream to combine them.
+   * 
+   * NOTE: This calls duplicate() on the ByteBuffer to prevent modifying the original
+   */
+  def apply(bufs: Seq[ByteBuffer]): InputStream = {
+    if (bufs.size == 1) new ByteBufferInputStream(bufs.head.duplicate())
+    else new SequenceInputStream(bufs.map{ buf: ByteBuffer => new ByteBufferInputStream(buf.duplicate()) }.iterator.asJavaEnumeration)
+  }
+}
 
 /**
  * A Simple InputStream wrapper around a ByteBuffer
  */
 final class ByteBufferInputStream(buf: ByteBuffer) extends InputStream {
+  override def markSupported(): Boolean = true
+  
+  override def mark(readlimit: Int): Unit = buf.mark()
+  
+  override def reset(): Unit = try {
+    buf.reset()
+  } catch {
+    // InputStream specifies that we throw an IOException "if the stream has
+    // not been marked or if the mark has been invalidated"
+    case ex: InvalidMarkException => throw new IOException(ex)
+  }
+  
   def read(): Int = {
     if (!buf.hasRemaining()) return -1
     buf.get() & 0xFF
