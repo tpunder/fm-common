996b465f889222554355c85fdcef60b6a407aab4
Tim Underwood <timunderwood@gmail.com>
2014-10-15 20:32:26 -0700

    Added DigestUtils helpers that take a File as an argument

--- src/main/scala/fm/common/DigestUtils.scala
+++ src/main/scala/fm/common/DigestUtils.scala
@@ -15,7 +15,7 @@
  */
 package fm.common
 
-import java.io.InputStream
+import java.io.{File, FileInputStream, InputStream}
 import org.apache.commons.codec.digest.{DigestUtils => Apache}
 
 /**
@@ -25,24 +25,30 @@ object DigestUtils {
   def md5(data: Array[Byte]): Array[Byte] = Apache.md5(data)
   def md5(data: InputStream): Array[Byte] = Apache.md5(data)
   def md5(data: String)     : Array[Byte] = Apache.md5(data)
+  def md5(file: File)       : Array[Byte] = Resource.using(new FileInputStream(file)){ md5 }
   
   def md5Hex(data: Array[Byte]): String = Apache.md5Hex(data)
   def md5Hex(data: InputStream): String = Apache.md5Hex(data)
   def md5Hex(data: String)     : String = Apache.md5Hex(data)
+  def md5Hex(file: File)       : String = Resource.using(new FileInputStream(file)){ md5Hex }
   
   def sha1(data: Array[Byte]): Array[Byte] = Apache.sha1(data)
   def sha1(data: InputStream): Array[Byte] = Apache.sha1(data)
   def sha1(data: String)     : Array[Byte] = Apache.sha1(data)
+  def sha1(file: File)       : Array[Byte] = Resource.using(new FileInputStream(file)){ sha1 }
   
   def sha1Hex(data: Array[Byte]): String = Apache.sha1Hex(data)
   def sha1Hex(data: InputStream): String = Apache.sha1Hex(data)
   def sha1Hex(data: String)     : String = Apache.sha1Hex(data)
+  def sha1Hex(file: File)       : String = Resource.using(new FileInputStream(file)){ sha1Hex }
   
   def sha256(data: Array[Byte]): Array[Byte] = Apache.sha256(data)
   def sha256(data: InputStream): Array[Byte] = Apache.sha256(data)
   def sha256(data: String)     : Array[Byte] = Apache.sha256(data)
+  def sha256(file: File)       : Array[Byte] = Resource.using(new FileInputStream(file)){ sha256 }
   
   def sha256Hex(data: Array[Byte]): String = Apache.sha256Hex(data)
   def sha256Hex(data: InputStream): String = Apache.sha256Hex(data)
   def sha256Hex(data: String)     : String = Apache.sha256Hex(data)
+  def sha256Hex(file: File)       : String = Resource.using(new FileInputStream(file)){ sha256Hex }
 }
