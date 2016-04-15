765f21057e38727d01d145b2bf3f8dd6c4418c48
Tim Underwood <timunderwood@gmail.com>
2016-04-15 14:59:55 -0700

    Attempt to cleanup the various lowerAlphanumeric implementations.  Also added accent stripping as part of the lowerAlphanumeric implementation

--- src/test/scala/fm/common/TestNormalize.scala
+++ src/test/scala/fm/common/TestNormalize.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Frugal Mechanic (http://frugalmechanic.com)
+ * Copyright 2016 Frugal Mechanic (http://frugalmechanic.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +33,7 @@ class TestNormalize extends FunSuite with Matchers {
     t("  foo  BAR  " -> "foo bar")
     t("Dorman HELP!" -> "dorman help")
     t("Dorman HELP\\!" -> "dorman help")
+    t("\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5" -> "aaaaaa")
   }
   
   test("lowerAlphanumeric") {
@@ -51,6 +52,31 @@ class TestNormalize extends FunSuite with Matchers {
     t("Dorman HELP\\!" -> "dormanhelp")
     t("dorman HELP\\!" -> "dormanhelp")
     t("doRman HELP\\!" -> "dormanhelp")
+    t("\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5" -> "aaaaaa")
+  }
+  
+  test("lowerAlphanumericWithPositions") {
+    def t(str: String, normalized: String, positions: Array[Int]) {
+      val res: (String,Array[Int]) = Normalize.lowerAlphanumericWithPositions(str)
+      
+      (res._1, res._2.toIndexedSeq) should equal ((normalized, positions.toIndexedSeq))
+    }
+    
+    t("", "", Array())
+    t("Foo", "foo", Array(0,1,2))
+    t("  Foo  ", "foo", Array(2,3,4))
+    t(" - Foo - ", "foo", Array(3,4,5))
+    t("foo BAR", "foobar", Array(0,1,2,4,5,6))
+    t("  foo  BAR  ", "foobar", Array(2,3,4,7,8,9))
+    t("Dorman HELP!", "dormanhelp", Array(0,1,2,3,4,5,7,8,9,10))
+    t("Dorman HELP\\!", "dormanhelp", Array(0,1,2,3,4,5,7,8,9,10))
+    t("dorman HELP\\!", "dormanhelp", Array(0,1,2,3,4,5,7,8,9,10))
+    t("doRman HELP\\!", "dormanhelp", Array(0,1,2,3,4,5,7,8,9,10))
+    
+    t("foo", "foo", Array(0,1,2))
+    t("dormanhelp", "dormanhelp", Array(0,1,2,3,4,5,6,7,8,9))
+    t("dorman123help", "dorman123help", Array(0,1,2,3,4,5,6,7,8,9,10,11,12))
+    t("\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5", "aaaaaa", Array(0,1,2,3,4,5))
   }
   
   test("lowerAlphanumeric - Already Normalized - Should eq the original string") {
@@ -83,6 +109,8 @@ class TestNormalize extends FunSuite with Matchers {
     Normalize.reverseLowerAlphanumeric("BOSCH -", "bosch") should equal (Some("BOSCH"))
     Normalize.reverseLowerAlphanumeric(" BOSCH - ", "bosch") should equal (Some("BOSCH"))
     Normalize.reverseLowerAlphanumeric(" - BOSCH - ", "bosch") should equal (Some("BOSCH"))
+    
+    Normalize.reverseLowerAlphanumeric(" - \u00C0\u00C1\u00C2\u00C3\u00C4\u00C5 - ", "aaaaaa") should equal (Some("\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5"))
   }
   
   test("urlname") {
@@ -99,5 +127,6 @@ class TestNormalize extends FunSuite with Matchers {
     t("Dorman HELP!" -> "dorman-help")
     t("Dorman HELP\\!" -> "dorman-help")
     t("\\\\foo_bar\\asd//\\" -> "foo-bar-asd")
+    t("\\\\foo_bar\\\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5\\asd//\\" -> "foo-bar-aaaaaa-asd")
   }
 }
