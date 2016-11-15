710418a45c85c2d419d3209c851b6dfa80cb639d
Tim Underwood <timunderwood@gmail.com>
2016-11-15 13:51:09 -0800

    RichJVMString.toLocaleOption now filters out Locales that are not valid

--- jvm/src/main/scala/fm/common/rich/RichJVMString.scala
+++ jvm/src/main/scala/fm/common/rich/RichJVMString.scala
@@ -1,10 +1,10 @@
 package fm.common.rich
 
-// not scala-js compatible
-import java.util.Locale
+import fm.common.Implicits._
+import java.util.Locale // not scala-js compatible
 
 final class RichJVMString(val s: String) extends AnyVal {
   // Parse a language tag to a locale - Using Locale.Builder() instead of Locale.forLanguageTag catches malformed strings
-  def toLocaleOption: Option[Locale] = scala.util.Try{ new Locale.Builder().setLanguageTag(s).build() }.toOption
+  def toLocaleOption: Option[Locale] = scala.util.Try{ new Locale.Builder().setLanguageTag(s).build() }.toOption.filter{ _.isValid }
   def toLocale: Locale = toLocaleOption.getOrElse(throw new Exception(s"Invalid locale language tag: $s"))
 }
