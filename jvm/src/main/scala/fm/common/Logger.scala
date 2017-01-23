190e07b57c4b535498dc545d02745066a124e33f
Tim Underwood <timunderwood@gmail.com>
2017-01-23 10:26:38 -0800

    SLF4J Logging should be conditional!!  (otherwise what’s the point of taking a by-name param)

--- jvm/src/main/scala/fm/common/Logger.scala
+++ jvm/src/main/scala/fm/common/Logger.scala
@@ -65,17 +65,17 @@ object Logger {
   final class SLF4JLogger(self: org.slf4j.Logger) extends Logger {
     def underlying: org.slf4j.Logger = self
     
-    def isTraceEnabled: Boolean = self.isTraceEnabled()
-    def isDebugEnabled: Boolean = self.isDebugEnabled()
-    def isInfoEnabled : Boolean = self.isInfoEnabled()
-    def isWarnEnabled : Boolean = self.isWarnEnabled()
-    def isErrorEnabled: Boolean = self.isErrorEnabled()
+    @inline def isTraceEnabled: Boolean = self.isTraceEnabled()
+    @inline def isDebugEnabled: Boolean = self.isDebugEnabled()
+    @inline def isInfoEnabled : Boolean = self.isInfoEnabled()
+    @inline def isWarnEnabled : Boolean = self.isWarnEnabled()
+    @inline def isErrorEnabled: Boolean = self.isErrorEnabled()
     
-    def trace(msg: => String): Unit = self.trace(msg)
-    def debug(msg: => String): Unit = self.debug(msg)
-    def info(msg: => String) : Unit = self.info(msg)
-    def warn(msg: => String) : Unit = self.warn(msg)
-    def error(msg: => String): Unit = self.error(msg)
+    def trace(msg: => String): Unit = if (isTraceEnabled) self.trace(msg)
+    def debug(msg: => String): Unit = if (isDebugEnabled) self.debug(msg)
+    def info(msg: => String) : Unit = if (isInfoEnabled) self.info(msg)
+    def warn(msg: => String) : Unit = if (isWarnEnabled) self.warn(msg)
+    def error(msg: => String): Unit = if (isErrorEnabled) self.error(msg)
     
     def trace(ex: Throwable): Unit = self.trace("Caught Exception", ex)
     def debug(ex: Throwable): Unit = self.debug("Caught Exception", ex)
@@ -83,11 +83,11 @@ object Logger {
     def warn(ex: Throwable) : Unit = self.warn("Caught Exception", ex)
     def error(ex: Throwable): Unit = self.error("Caught Exception", ex)
     
-    def trace(msg: => String, ex: Throwable): Unit = self.trace(msg, ex)
-    def debug(msg: => String, ex: Throwable): Unit = self.debug(msg, ex)
-    def info(msg: => String, ex: Throwable) : Unit = self.info(msg, ex)
-    def warn(msg: => String, ex: Throwable) : Unit = self.warn(msg, ex)
-    def error(msg: => String, ex: Throwable): Unit = self.error(msg, ex)
+    def trace(msg: => String, ex: Throwable): Unit = if (isTraceEnabled) self.trace(msg, ex)
+    def debug(msg: => String, ex: Throwable): Unit = if (isDebugEnabled) self.debug(msg, ex)
+    def info(msg: => String, ex: Throwable) : Unit = if (isInfoEnabled) self.info(msg, ex)
+    def warn(msg: => String, ex: Throwable) : Unit = if (isWarnEnabled) self.warn(msg, ex)
+    def error(msg: => String, ex: Throwable): Unit = if (isErrorEnabled) self.error(msg, ex)
   }
 }
 
